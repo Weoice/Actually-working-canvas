8583886dce12b207dc8bc0e44187247ed875d2cf
Weoice <84687104+Weoice@users.noreply.github.com>
2022-10-21 22:13:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
 
 			alphabetkey();
 			document.getElementById("d1").innerHTML="You Pressed Alphabet Key!";
@@ -42,7 +42,25 @@ function my_keydown(e)
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
+
+	if((keyPressed >=48 && keyPressed<=57)) {
+
+		numberkey();
+		document.getElementById("d1").innerHTML="You Pressed number Key!";
+		console.log("number key");
+	}
+
+	if((keyPressed >=37 && keyPressed<=40)) {
+
+		arrowkey();
+		document.getElementById("d1").innerHTML="You Pressed arrow Key!";
+		console.log("arrow key");
+	}
+	
+
+    
 }
+
 
 function alphabetkey()
 {
